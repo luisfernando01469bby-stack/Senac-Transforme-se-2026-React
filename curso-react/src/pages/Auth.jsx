d028e5de504c023285f8b70344ac958964ce4bb0
LuisFernando44 <luisfernando01469bby@gmail.com>
2026-09-02 20:13:11 -0300

    batatinha didatica

--- curso-react/src/pages/Auth.jsx
+++ curso-react/src/pages/Auth.jsx
@@ -1,12 +1,24 @@
+import { useState } from 'react';
 import {Link} from 'react-router'
 
 function Auth() {
+    const [batatinha, setbatatinha] = useState(2);
+
+    function sub(){
+        setbatatinha(batatinha - 1)
+    }
     return (
         <>
             <nav className=" p-3 shadow-md">
                 <Link className="p-2 mr-2 hover:bg-primary" to="/">Voltar</Link>
+
+                <div className="bg-red-100 rounded-full p-2" onClick ={sub}>-</div>
+                {batatinha}
+                 <div className="bg-green-100 rounded-full p-2" onClick ={() =>setbatatinha(batatinha +1)}>+</div>
             </nav>
-            <div className="max-w-lg mx-auto p-6 bg-blue-100 ">
+
+
+            <div className="max-w-lg mx-auto p-6 bg-blue-100   ">
                 <form className='flex flex-col gap-[20]'>
                     <h2 className="font-size">Login</h2>
 
